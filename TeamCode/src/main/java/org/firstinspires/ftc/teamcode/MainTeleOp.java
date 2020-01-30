d5911244156fc7921934984d7d47aab2883243b9
Sahana M <ssmoogi@gmail.com>
2020-01-29 22:47:37 -0800

    Update MainTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -16,7 +16,7 @@ public class MainTeleOp extends LinearOpMode {
     private Servo leftextender, rightextender, leftrotater, rightrotater, centerrotater, grabber1, grabber2;
 
     double left_trigger_value = 0;
-
+    /*
     double leftextenderposition = 0;
     double rightextenderposition = 0;
     double leftrotaterposition = 0;
@@ -28,9 +28,7 @@ public class MainTeleOp extends LinearOpMode {
     double grabber1position = 0;
     double grabber2position = 0;
     double grabberincrement = 0.01;
-
-
-
+    */
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
@@ -45,7 +43,7 @@ public class MainTeleOp extends LinearOpMode {
         rightslide = hardwareMap.get(DcMotor.class, "right_slide");
         //arm = hardwareMap.get(DcMotor.class, "arm");
 
-
+        /*
         leftextender = hardwareMap.get(Servo.class, "left_extender");
         rightextender = hardwareMap.get(Servo.class, "right_extender");
         leftrotater = hardwareMap.get(Servo.class, "left_rotater");
@@ -53,7 +51,7 @@ public class MainTeleOp extends LinearOpMode {
         centerrotater = hardwareMap.get(Servo.class, "center_rotater");
         grabber1 = hardwareMap.get(Servo.class, "grabber1");
         grabber2 = hardwareMap.get(Servo.class, "grabber2");
-
+        */
 
         fldrive.setDirection(DcMotor.Direction.FORWARD);
         frdrive.setDirection(DcMotor.Direction.REVERSE);
@@ -63,6 +61,9 @@ public class MainTeleOp extends LinearOpMode {
         leftslide.setDirection(DcMotor.Direction.FORWARD);
         rightslide.setDirection(DcMotor.Direction.REVERSE);
         //arm.setDirection(DcMotor.Direction.FORWARD);
+
+        leftslide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightslide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         leftslide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightslide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -75,7 +76,9 @@ public class MainTeleOp extends LinearOpMode {
 
             double drivepower;
             double flypower = 1;
-            double liftpower = 1;
+            //double liftpower = 1;
+            double liftpower = gamepad2.left_stick_y;
+            int liftincrement = 50;
             //double armpower = 1;
 
             left_trigger_value = gamepad1.left_trigger-0.5;
@@ -94,7 +97,6 @@ public class MainTeleOp extends LinearOpMode {
             double b_left;
             double b_right;
             double fly_wheel;
-            double fly_wheel_rev;
             double left_slide;
             double right_slide;
             //double arm;
@@ -103,8 +105,7 @@ public class MainTeleOp extends LinearOpMode {
             f_right = DriveSpeed - Rotate - Strafe;
             b_right = DriveSpeed - Rotate + Strafe;
             b_left = DriveSpeed + Rotate - Strafe;
-            fly_wheel = FlySpeed;
-            fly_wheel_rev = FlySpeedReverse;
+            fly_wheel = FlySpeed - FlySpeedReverse;
             left_slide = Lift;
             right_slide = Lift;
             //arm = MoveArm;
@@ -114,25 +115,63 @@ public class MainTeleOp extends LinearOpMode {
             brdrive.setPower(Range.clip(b_right, -drivepower, drivepower));
             bldrive.setPower(Range.clip(b_left, -drivepower, drivepower));
             flywheel.setPower(Range.clip(fly_wheel, -flypower, flypower));
-            flywheel.setPower(Range.clip(fly_wheel_rev, flypower, -flypower));
 
-            if (leftslide.getCurrentPosition() > rightslide.getCurrentPosition()) {
+
+            if(liftpower > 0) {
+                leftslide.setTargetPosition(leftslide.getCurrentPosition() + liftincrement);
+                rightslide.setTargetPosition(leftslide.getCurrentPosition() + liftincrement);
+
+                leftslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                rightslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                leftslide.setPower(liftpower);
+                rightslide.setPower(liftpower);
+
+                while (leftslide.isBusy() && rightslide.isBusy()){
+                    //wait
+                }
+
+                leftslide.setPower(0);
+                rightslide.setPower(0);
+            } else if(liftpower < 0) {
+                leftslide.setTargetPosition(leftslide.getCurrentPosition() - liftincrement);
+                rightslide.setTargetPosition(leftslide.getCurrentPosition() - liftincrement);
+
+                leftslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                rightslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                leftslide.setPower(liftpower);
+                rightslide.setPower(liftpower);
+
+                while (leftslide.isBusy() && rightslide.isBusy()){
+                    //wait
+                }
+
+                leftslide.setPower(0);
+                rightslide.setPower(0);
+            } else{
+                //no action
+            }
+
+
+
+            /*if (rightslide.getCurrentPosition() > leftslide.getCurrentPosition()) {
                 while (leftslide.getCurrentPosition() != rightslide.getCurrentPosition()) {
-                    rightslide.setPower(0.8);
-                    leftslide.setPower(0);
+                    leftslide.setPower(1);
+                    rightslide.setPower(0);
                 }
                 rightslide.setPower(0);
                 leftslide.setPower(0);
             }
-            else {
+            else if (leftslide.getCurrentPosition() == rightslide.getCurrentPosition()){
                 leftslide.setPower(Range.clip(left_slide, -liftpower, liftpower));
                 rightslide.setPower(Range.clip(right_slide, -liftpower, liftpower));
-            }
+            }*/
 
             //arm.setPower(Range.clip(arm, -armpower, armpower));
 
 
-            leftextenderposition = leftextender.getPosition() - (gamepad2.right_trigger * 0.1);
+            /*leftextenderposition = leftextender.getPosition() - (gamepad2.right_trigger * 0.1);
             leftextender.setPosition(leftextenderposition);
 
             rightextenderposition = rightextender.getPosition() + (gamepad2.right_trigger * 0.1);
@@ -172,7 +211,7 @@ public class MainTeleOp extends LinearOpMode {
                 grabber2position = grabber2.getPosition() + grabberincrement;
                 grabber1.setPosition(grabber1position);
                 grabber2.setPosition(grabber2position);
-            }
+            }*/
 
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
